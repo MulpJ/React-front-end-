fb7fac3b32059b0539719ed9c86633cf0396c060
matiasJoao <joaovictor.a4@gmail.com>
2022-03-22 02:15:43 -0300

    home feita
    home feita

--- mulpj-app/src/componentes/HomePage/home.js
+++ mulpj-app/src/componentes/HomePage/home.js
@@ -1,50 +1,65 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import logo from "../tela de login/image/logo.png"
+// import Swiper core and required modules
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import {Autoplay, Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import './home.css'
 import rick from "./image/rick.jpg"
 import inter from "./image/inter.jpg"
 import hobbit from "./image/hobbit.jpg"
 import mad from "./image/mad.jpg"
-import './home.css';
+import { Link } from "react-router-dom";
+import logo from "../tela de login/image/logo.png"
+import it from './image/it.jpg'
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
+export function HomePage() {
 
-export function HomePage () {
-    let time = 2000,
-        currentImageIndex = 0,
-        images = document.querySelectorAll("#imgs img"),
-        max = images.length;
-            console.log(images)
-        function nextImage (){
-            images[currentImageIndex].classList.remove("selected")
-            currentImageIndex++
-            if (currentImageIndex >= max){
-                currentImageIndex = 0
-            }
-                images[currentImageIndex].classList.add("selected")
-        }
-        function start (){
-         setInterval(() => {
-                nextImage ()
-         }, time)
-        }
+  const [imgs ,Setimgs] = useState([])
 
-        window.addEventListener("load", start)
+  useEffect(() => {
+    for (let i; i > 4; i++) {
+      Setimgs('https://picsum.photos/1920/1080')
+    }
+  },[])
 
-    return (
+  return (
+    <Swiper
+    className='ma'
+      // install Swiper modules
+      modules={[Autoplay, Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={0}
+      slidesPerView={1}
+      autoplay={{delay: 3000}}
+     
+    >
         <header className="header">
-            <Link to="/login">Entrar</Link>
+            <Link className="lon"to="/login">Entrar</Link>
              <img className="logo" src={logo}></img><p className="nome">MulpJ</p>
-            <div className="img" id="imgs">
-                <img className="selected" src={mad}></img>
-                <img  src={rick}></img>
-                <img  src={inter}></img>
-                <img  src={hobbit}></img>
-            </div>
-            <h1>Melhor Plataforma de Filmes e series</h1>
-                <p className="sexo">Assine por 29,99*</p>
-                <p className="desc">Em breve*</p>   
+             <h1>Melhor Plataforma de Filmes e series</h1>
+                <Link className="se" to="/cadastro" >Cadastre-se Agora</Link>
+                <p className="desc">Filmes e series gratuitos!</p>   
         </header>
-    )
-    
-    
-}
+      <SwiperSlide>
+          <img className='background' src={rick}/>
+      </SwiperSlide>
+      <SwiperSlide>
+          <img className='background'src={hobbit}/>
+      </SwiperSlide>
+      <SwiperSlide>
+          <img className='background'src={inter}/>
+      </SwiperSlide>
+      <SwiperSlide>
+          <img className='background' src={mad}/>
+      </SwiperSlide>
+      <SwiperSlide>
+          <img className='background' src={it}/>
+      </SwiperSlide>
+    </Swiper>
+  );
+};
